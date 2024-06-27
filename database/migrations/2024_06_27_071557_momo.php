24580692c8c22c3d5a4150f40bce87579dca3496
thuongatt222 <thuongatt2k3@gmail.com>
2024-06-27 07:23:02 +0700

    27/6

---
+++ database/migrations/2024_06_27_071557_momo.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('momo', function(Blueprint $table){
+            $table->id();
+            $table->string('partnerCode');
+            $table->string('orderId');
+            $table->string('requestId');
+            $table->string('amount');
+            $table->string('orderInfo');
+            $table->string('orderType');
+            $table->string('transId');
+            $table->string('payType');
+            $table->string('signature');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
